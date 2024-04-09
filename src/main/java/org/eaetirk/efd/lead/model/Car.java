92a612d27a5484f1928816ba4e4de2d6a98d8f18
IremGA <iremgamzeli@gmail.com>
2024-04-09 07:53:07 +0200

    Changes have been applied

--- src/main/java/org/eaetirk/efd/lead/model/Car.java
+++ src/main/java/org/eaetirk/efd/lead/model/Car.java
@@ -1,6 +1,6 @@
 package org.eaetirk.efd.lead.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
