92083700aa043ab0274a6c078449a3c69462557d
IremGA <iremgamzeli@gmail.com>
2024-04-09 08:35:57 +0200

    Changes have been applied

--- src/main/java/org/eaetirk/efd/lead/model/Lead.java
+++ src/main/java/org/eaetirk/efd/lead/model/Lead.java
@@ -1,7 +1,14 @@
 package org.eaetirk.efd.lead.model;
 
 import jakarta.json.bind.annotation.JsonbProperty;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
