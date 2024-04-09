27cf7c7bf8492debac5bdd638cd7cfc3dfa5339f
IremGA <iremgamzeli@gmail.com>
2024-04-09 08:38:02 +0200

    Changes have been applied

--- src/main/java/org/eaetirk/efd/lead/model/LeadDeviceSpecification.java
+++ src/main/java/org/eaetirk/efd/lead/model/LeadDeviceSpecification.java
@@ -1,7 +1,14 @@
 package org.eaetirk.efd.lead.model;
 
 import jakarta.json.bind.annotation.JsonbTransient;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
